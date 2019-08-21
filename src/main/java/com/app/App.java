cf71f5af039c44bd6d73294035185cc5f23dd10b
train <train@TRN5474.dmztrn.amdocs.trn>
2019-08-21 16:29:56 +0530

    first commit for MyApp

---
+++ src/main/java/com/app/App.java
@@ -0,0 +1,20 @@
+package com.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is new message!" );
+	System.out.println( "App is invoking" );
+	update();
+
+    }
+    public void update()
+    {
+	System.out.println("App update!");
+    }
+}
